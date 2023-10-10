00e896af74335c95758b661e08f1e46b8cb33e4a
malo-romano <malo.romano@gmail.com>
2023-10-10 20:42:33 +0200

    PW01 is over

--- script.js
+++ script.js
@@ -1,17 +1,22 @@
-hasVoted = false;
+let hasVoted = false;
 
 function vote(name) {
-    if (!hasVoted) {
-
-        var voteUser = confirm("Souhaitez-vous voter pour " + name + " ?");
-        // Vérifiez la réponse de l'utilisateur
-        if (voteUser) {
-            // Si l'utilisateur a cliqué sur OK
-            document.getElementById("winner").innerText = name + " remporte cette édition de Mister HEIG-VD 2023 !"
-            document.getElementById(name).style='background-image : url("https://media.tenor.com/fasarUEeTfMAAAAM/flash-rainbow.gif"); color:white;';
-            hasVoted = true;
-        }
+    if (hasVoted) {
+        alert("VOUS AVEZ DÉJÀ VOTÉ ! TRICHEUR ! NOUS AVONS DES YEUX PARTOUT, ET NOUS NE TOLÉRERONS PAS VOTRE FRAUDE !");
+        return;
     }
 
+    const voteUser = confirm(`Souhaitez-vous voter pour ${name} ?`);
+    
+    if (voteUser) {
+        // Si l'utilisateur a cliqué sur OK
+        const winnerElement = document.getElementById("winner");
+        winnerElement.innerText = `${name} remporte cette édition de Mister HEIG-VD 2023 !`;
 
+        const nameElement = document.getElementById(name);
+        nameElement.style.backgroundImage = 'url("https://media.tenor.com/fasarUEeTfMAAAAM/flash-rainbow.gif")';
+        nameElement.style.color = 'white';
+
+        hasVoted = true;
+    }
 }
